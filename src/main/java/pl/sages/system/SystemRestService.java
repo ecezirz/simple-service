384ac3b3a67775c8aaf2839826b05b999069e976
Czarek <cezary.irzykowski@ericsson.com>
2015-06-30 11:48:21 +0200

    major bugs removed

--- src/main/java/pl/sages/system/SystemRestService.java
+++ src/main/java/pl/sages/system/SystemRestService.java
@@ -2,7 +2,7 @@ package pl.sages.system;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
-//import org.apache.commons.lang3.StringUtils;
+
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
@@ -14,18 +14,16 @@ public class SystemRestService
 	@Autowired
 	private SystemManager systemManager;
 
-	//@SuppressWarnings("null")
+	
 	@GET
 	@Path(value = "/ping")
 	@Produces(MediaType.APPLICATION_JSON)
 	public Response ping(@QueryParam("input") String input)
 	{
-		/*if(StringUtils.isNotBlank(input) && input.equals("czarek")){
-						String a = null;
-						a.toLowerCase();
-					}*/
+		
 		String result = systemManager.check(input);
-		return result.equals("OK!") ? Response.ok(result).build() :
+		
+		return "OK!".equals(result) ? Response.ok(result).build() :
 				Response.status(Response.Status.BAD_REQUEST).entity(result).build();
 	}
 }
