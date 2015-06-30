724ae24e15fd3f29da1de68f29d5b625f73f9297
Czarek <cezary.irzykowski@ericsson.com>
2015-06-30 09:37:12 +0200

    null pointer exception added

--- src/main/java/pl/sages/system/SystemRestService.java
+++ src/main/java/pl/sages/system/SystemRestService.java
@@ -2,6 +2,8 @@ package pl.sages.system;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
+import org.apache.commons.lang3.StringUtils;
+
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -12,11 +14,16 @@ public class SystemRestService
 	@Autowired
 	private SystemManager systemManager;
 
+	//@SuppressWarnings("null")
 	@GET
 	@Path(value = "/ping")
 	@Produces(MediaType.APPLICATION_JSON)
 	public Response ping(@QueryParam("input") String input)
 	{
+		if(StringUtils.isNotBlank(input) && input.equals("czarek")){
+						String a = null;
+						a.toLowerCase();
+					}
 		String result = systemManager.check(input);
 		return result.equals("OK!") ? Response.ok(result).build() :
 				Response.status(Response.Status.BAD_REQUEST).entity(result).build();
